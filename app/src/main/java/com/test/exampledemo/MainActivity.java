2c73f31cf42e60d1b7c8954be0ce925c87a3e2b9
junyu.wu <junyu.wu@datacvg.com>
2020-06-18 18:39:53 +0800

    feat: 添加handler消息分发demo

--- app/src/main/java/com/test/exampledemo/MainActivity.java
+++ app/src/main/java/com/test/exampledemo/MainActivity.java
@@ -1,30 +1,57 @@
 package com.test.exampledemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            Toast.makeText(MainActivity.this, String.valueOf(msg.what), Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initListener();
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(5000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        Message message = Message.obtain();
+                        message.what = 1;
+                        mHandler.sendMessage(message);
+                    }
+                }
+        ).start();
     }
 
     /**
      * 初始化监听器
      */
-    private void initListener(){
-        Button btnJump1=findViewById(R.id.btn_jump1);
+    private void initListener() {
+        Button btnJump1 = findViewById(R.id.btn_jump1);
         btnJump1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,SecondActivity.class));
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
             }
         });
     }
